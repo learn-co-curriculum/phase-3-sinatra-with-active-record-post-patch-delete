8ee2907cca866641b3237ffd0c32c1ee1288f2c4
ihollander <ianholla@gmail.com>
2021-07-20 10:55:23 -0400

    Added solution

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,4 +16,43 @@ class ApplicationController < Sinatra::Base
     })
   end
 
+  delete '/reviews/:id' do
+    # find the review using the ID
+    review = Review.find(params[:id])
+
+    # delete the review
+    review.destroy
+
+    # send a response with the deleted review as JSON
+    review.to_json
+  end
+
+  post '/reviews' do
+    # create a new review in the database
+    # params is a hash of key-value pairs coming from the body of the request
+    review = Review.create(
+      score: params[:score],
+      comment: params[:comment],
+      game_id: params[:game_id],
+      user_id: params[:user_id]
+    )
+
+    # send back a response with the created review as JSON
+    review.to_json
+  end
+
+  patch '/reviews/:id' do
+    # find the review using the ID
+    review = Review.find(params[:id])
+
+    # update the review in the database
+    review.update(
+      score: params[:score],
+      comment: params[:comment]
+    )
+
+    # send back the updated review as JSON
+    review.to_json
+  end
+
 end
