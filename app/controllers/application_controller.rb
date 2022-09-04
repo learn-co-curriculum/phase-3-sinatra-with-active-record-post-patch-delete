7daacd4f49f85243be89b4dced36572b991799ce
Silvia-Mutete <silvia.mutete@student.moringaschool.com>
2022-09-04 19:01:20 +0300

    commit tree 24b6d405cce7b58850150a28e611753cddb8281d

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,12 +8,36 @@ class ApplicationController < Sinatra::Base
 
   get '/games/:id' do
     game = Game.find(params[:id])
-
     game.to_json(only: [:id, :title, :genre, :price], include: {
       reviews: { only: [:comment, :score], include: {
         user: { only: [:name] }
       } }
     })
+  end
+
+  delete '/reviews/:id' do
+    review = Review.find(params[:id])
+    review.destroy
+    review.to_json
+  end
+
+  post '/reviews' do
+    review = Review.create(
+      score: params[:score],
+      comment: params[:comment],
+      game_id: params[:game_id],
+      user_id: params[:user_id]
+    )
+    review.to_json
+  end
+
+  patch '/reviews/:id' do
+    review = Review.find(params[:id])
+    review.update(
+      score: params[:score],
+      comment: params[:comment]
+    )
+    review.to_json
   end
 
 end
